6c40f8f5a27c73645916faa478c064df967cff7b
홍은표 <44293278+potato179@users.noreply.github.com>
2021-05-16 17:09:05 +0900

    DB 개설 완료

---
+++ ps_questions.js
@@ -0,0 +1,101 @@
+const mysqlconfig = require("./js/mysql_con.js");
+var con = mysqlconfig.con;
+
+function get_questions(req, res, next){
+    var s = `select * from questions`;
+    con.query(s, function(err, result){
+        if(err) throw err;
+        res.send(result);
+    });
+}
+
+function view_question(req, res, next){
+    console.log(`id: `, req.query.id);
+    var s = `select * from users where id = "${req.query.id}";`;
+    con.query(s, function(err, result){
+        if(err) throw err;
+        if(result[0] == undefined){
+            return res.send(`존재하지 않는 질문입니다.`);
+        }
+        res.send(result[0]);
+    });
+}
+
+function modify_question(req, res, next){
+    var ct = req.query.content;
+    var id = req.query.id;
+    var s = `update question set content = "${ct} where id = "${id}`;
+    con.query(s, function(err, result){
+        if(err) throw err;
+        console.log(result);
+        res.send({
+            condition: "등록됨",
+            message: "질문 수정을 완료하였습니다."
+        });
+    });
+}
+
+function write_question(req, res, next){
+    var tit = req.query.title;
+    var wtr = req.query.writer;
+    var ct = req.query.content;
+    var ca = req.query.category;
+    var s = `insert into questions (title, writer, content, category) values("${tit}", "${wtr}", "${ct}", "${ca}");`;
+    con.query(s, function(err, result){
+        if(err) throw err;
+        console.log(result);
+        res.send({
+            condition: "등록됨",
+            message: "질문 등록을 완료하였습니다."
+        });
+    });
+}
+
+function delete_question(req, res, next){
+    var id = req.query.id;
+    var s = `delete from question where id = "${id}`;
+    con.query(s, function(err, result){
+        if(err) throw err;
+        console.log(result);
+        res.send({
+            condition: "삭제됨",
+            message: "질문 삭제를 완료하였습니다."
+        });
+    });
+}
+
+function write_comment(req, res, next){
+    var com = req.query.comments;
+    var id = req.query.id;
+    var email = req.query.email;
+    var username = req.query.username;
+    var s = `select comment from questions where id = "${id}";`;
+    con.query(s, function(err, result){
+        if(err) throw err;
+        //json 파싱할 것.
+    });
+}
+
+function like_question(req, res, next){
+    var email = req.query.email;
+    var id = req.query.id;
+    console.log(id);
+    var s = `select likes from questions where id = "${id}";`;
+    con.query(s, function(err, result){
+        if(err) throw err;
+        //이것도 json으로 ㄱ
+    });
+}
+
+function write_question_page(req, res, next){
+    res.sendfile("write_question.html", {root: __dirname});
+}
+
+exports.get_questions = get_questions;
+exports.view_question = view_question;
+exports.modify_question = modify_question;
+exports.write_question = write_question;
+exports.delete_question = delete_question;
+exports.write_comment = write_comment;
+exports.like_question = like_question;
+exports.write_question_page = write_question_page;
--- server.js
+++ server.js
@@ -1,52 +1,56 @@
 const http = require("http");
-const mysql_con = require("./js/mysql_con.js");
 const express = require("express");
-const cookieParser = require("cookie-parser");
 const fs = require("fs");
 const app = express();
 
-const ps_member = require("./ps_member.js");
+const mysqlconfig = require("./js/mysql_con.js");
+const con = mysqlconfig.con;
 
-const hostname = '127.0.0.1';
-var port = 5500;
+const ps_users = require("./ps_member.js");
+const ps_questions = require("./ps_questions");
 
-var url_list = [
-    {url: "/login", ps: ps_member.login},
-    {url: "/login.html", ps: ps_member.login_html},
-    {url: "/logout", ps: ps_member.logout},
-    {url: "/join", ps: ps_member.join},
-    {url: "/join.html", ps: ps_member.join_html},
+const hostname = "127.0.0.1";
+const port = "3000";
+
+var urls = [
+    {url: "/login", ps: ps_users.login},
+    {url: "/login.html", ps: ps_users.login_html},
+    {url: "/logout", ps: ps_users.logout},
+    {url: "/join", ps: ps_users.join},
+    {url: "/join.html", ps: ps_users.join_html},
+    {url: "/get_questions", ps: ps_questions.get_questions},
+    {url: "/view_question", ps: ps_questions.view_question},
+    {url: "/modify_question", ps: ps_questions.modify_question},
+    {url: "/write_question", ps: ps_questions.write_question},
+    {url: "/delete_question", ps: ps_questions.delete_question}
 ];
 
-process.argv.forEach(function(item, index){
-
-    console.log("argv %d, %s", index, item);
-
-    if( item == '--port'){
-        port = Number(process.argv[index + 1]);
-    }
-
+process.argv.forEach(function(item, index) {
+    console.log(item, index);
+    if(item == "--port") port = Number(process.argv[index + 1]);
 });
 
-var con = mysql_con.con;
-
-app.use('/public', express.static('public'));
-
+app.use("/public", express.static("public"));
 app.listen(port, hostname, () => {
-    console.log(`http://${hostname}:${port}/ 에서 서버 작동중`);
+    console.log(port, hostname);
 });
 
-
-app.get('/', function(req, res, next){
-    res.sendFile('lesson.html', {root: __dirname});
+app.get("/", function(req, res, next){
+    res.sendfile("index.html", {root: __dirname});
 });
 
-app.get('/lesson.html', function(req, res, next){
-    res.sendFile('lesson.html', {root: __dirname});
-});  
+app.get("/index.html", function(req, res, next){
+    res.sendfile("index.html", {root: __dirname});
+});
 
-url_list.forEach(function(element, index){
+app.get("/community.html", function(req, res, next){
+    res.sendfile("community.html", {root: __dirname});
+});
 
+app.get("/codingquiz.html", function(req, res, next){
+    res.sendfile("codingtest.html", {root: __dirname});
+})
+
+urls.forEach(function(element, index){
     app.get(element.url, element.ps);
-
 });
